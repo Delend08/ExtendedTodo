a3f04e5a50aebf938ed1b290ceac30341a80954a
Delend <delend018@gmail.com>
2026-04-09 23:21:19 +0900

    TodoList 구현

--- src/popup/components/TodoList.tsx
+++ src/popup/components/TodoList.tsx
@@ -1,1 +1,31 @@
-// TODO: TodoList 컴포넌트 (todos 배열을 TodoItem으로 렌더링)
+import type { Todo } from "../types/todo";
+import TodoItem from "./TodoItem";
+
+interface TodoListProps {
+    todos: Todo[];
+    onToggle: (id: string) => void;
+    onDelete: (id: string) => void;
+    onUpdate: (todo: Todo) => void;
+}
+
+function TodoList({ todos, onToggle, onDelete, onUpdate }: TodoListProps) {
+    if (todos.length === 0) {
+        return <p>아직 등록된 할 일이 없어요.</p>;
+    }
+
+    return (
+        <ul>
+            {todos.map((todo) => (
+                <TodoItem
+                    key={todo.id}
+                    todo={todo}
+                    onToggle={onToggle}
+                    onDelete={onDelete}
+                    onUpdate={onUpdate}
+                />
+            ))}
+        </ul>
+    );
+}
+
+export default TodoList;
