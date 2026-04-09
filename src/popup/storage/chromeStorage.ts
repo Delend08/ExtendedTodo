4b189b8960284dae94ebbe1931b841981bb731b7
Delend <delend018@gmail.com>
2026-04-09 22:32:19 +0900

    할 일 저장 및 불러오기 로직

--- src/popup/storage/chromeStorage.ts
+++ src/popup/storage/chromeStorage.ts
@@ -1,3 +1,12 @@
 import type { Todo } from "../types/todo";
 
 // TODO: loadTodos, saveTodos 구현
+export const loadTodos = async (): Promise<Todo[]> => {
+  const result = await chrome.storage.local.get(["todos"]);
+
+  return Array.isArray(result.todos) ? result.todos : [] as Todo[];
+};
+
+export const saveTodos = async (todos: Todo[]): Promise<void> => {
+  await chrome.storage.local.set({ todos });
+};
