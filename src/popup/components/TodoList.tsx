21a1f93903ab595fea08b909fa677fa2bdd7c4fd
Delend <delend018@gmail.com>
2026-04-10 00:45:36 +0900

    할 일 정렬 css 변경

--- src/popup/components/TodoList.tsx
+++ src/popup/components/TodoList.tsx
@@ -13,9 +13,23 @@ function TodoList({ todos, onToggle, onDelete, onUpdate }: TodoListProps) {
         return <p className="empty-state">아직 등록된 할 일이 없어요.</p>;
     }
 
+    const getDueTimestamp = (dueDate: string | null): number => {
+        if (!dueDate) {
+            return Number.POSITIVE_INFINITY;
+        }
+        const dueTimestamp = new Date(dueDate).getTime();
+        return Number.isNaN(dueTimestamp) ? Number.POSITIVE_INFINITY : dueTimestamp;
+    };
+    const orderedTodos = [...todos].sort((a, b) => {
+        if (a.isCompleted !== b.isCompleted) {
+            return Number(a.isCompleted) - Number(b.isCompleted);
+        }
+        return getDueTimestamp(a.dueDate) - getDueTimestamp(b.dueDate);
+    });
+
     return (
         <ul className="todo-list">
-            {todos.map((todo) => (
+            {orderedTodos.map((todo) => (
                 <TodoItem
                     key={todo.id}
                     todo={todo}
